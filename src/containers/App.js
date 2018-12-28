ad35af7cb0035aba421821623ba6365d5f676c4e
Prince <davidikede24@gmail.com>
2018-12-28 07:20:01 -0500

    added redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,69 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry'
+import { setSearchField, requestRobots}  from '../action';
+
+const mapStateToProps =(state) =>{
+    return {
+        // searchField: state.searchField    //if only one property (or reducer) searrchfield
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}//mapStateToProps= what state to listen to and send down as props 
+
+const mapDispatchToProps = (dispatch) =>{
+    return{
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)), // could be  any name not just  onSearchChange. look at tutorial video
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}//mapDispatchToProps= what props to listen to that are actions that should be dispatched
 
 class App extends Component{
-    constructor(){
-        super()
-        this.state ={
-                        robots: [],
-                        searchfield: ''
-                    }
-    }
+    // constructor(){
+    //     super()
+    //     this.state ={
+    //                     robots: [],
+    //                 }
+    // }
 
-    onSearchChange=(event)=>{
-        // console.log(event.target.value)
+    // onSearchChange=(event)=>{
+    //     // console.log(event.target.value)
 
-        this.setState({searchfield: event.target.value})  //change state 
-    }
+    //     this.setState({searchfield: event.target.value})  //change state 
+    // } 
+
     componentDidMount(){
         // alert("mounted")
- 
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response =>response.json())
-            .then(users => this.setState({robots: users}))
+        this.props.onRequestRobots();
+
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robot=>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        })
-        if (!this.state.robots.length){
-            return <h1>Loading</h1>
-        }
-        else{
-            return(
-                <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundry>
-                        <CardList robots={filteredRobots} />
-                    </ErrorBoundry>
-                </Scroll>
-                </div>
-            )
-        }
+        // const {robots} = this.state; //object detructoring
+        const {searchField, onSearchChange, robots, isPending} = this.props;   //object detructoring
 
+        const filteredRobots = robots.filter(robot  =>{
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+   
+        return isPending ? <h1>Loading</h1> : (
+                                                        <div className='tc'>
+                                                        <h1>RoboFriends</h1>
+                                                        <SearchBox searchChange={onSearchChange}/>
+                                                        <Scroll>
+                                                            <ErrorBoundry>
+                                                                <CardList robots={filteredRobots} />
+                                                            </ErrorBoundry>
+                                                        </Scroll>
+                                                        </div>
+                                                )
+                                                
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
